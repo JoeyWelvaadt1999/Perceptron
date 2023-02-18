457965bb4f60a8ba002b00c6afef9fc748eda78d
Joey1999 <joeywelvaadt@gmail.com>
2023-02-18 02:51:38 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,62 @@
+from neural import Layer, Network, Perceptron
+
+### Starting Assertion ###
+# Invert
+pInvert = Perceptron([-1], 1)
+assert pInvert.activation([0]) == 1
+assert pInvert.activation([1]) == 0
+
+# AND
+pAnd = Perceptron([1, 1], -1)
+assert pAnd.activation([1, 1]) == 1
+assert pAnd.activation([1, 0]) == 0
+assert pAnd.activation([0, 1]) == 0
+assert pAnd.activation([0, 0]) == 0
+
+# OR
+pOr = Perceptron([1, 1], -0.5)
+assert pOr.activation([1, 1]) == 1
+assert pOr.activation([1, 0]) == 1
+assert pOr.activation([0, 1]) == 1
+assert pOr.activation([0, 0]) == 0
+
+# Extended
+pExt = Perceptron([0.6, 0.3, 0.2], -0.4)
+assert pExt.activation([1,1,1]) == 1
+assert pExt.activation([1,1,0]) == 0
+assert pExt.activation([1,0,0]) == 0
+assert pExt.activation([1,0,1]) == 0
+assert pExt.activation([0,1,1]) == 0
+assert pExt.activation([0,1,0]) == 0
+assert pExt.activation([0,0,1]) == 0
+assert pExt.activation([0,0,0]) == 0
+
+# NAND
+pNand = Perceptron([-1,-1], 2)
+assert pNand.activation([1,1]) == 0
+assert pNand.activation([1,0]) == 1
+assert pNand.activation([0,1]) == 1
+assert pNand.activation([0,0]) == 1
+
+### Starting network test ###
+# XOR
+layer1 = Layer([pNand, pOr])
+layer2 = Layer([pAnd])
+network = Network([layer1, layer2])
+
+assert network.predict([1, 1]) == [0]
+assert network.predict([1, 0]) == [1]
+assert network.predict([0, 1]) == [1]
+assert network.predict([0, 0]) == [0]
+
+#Half adder
+pSum = Perceptron([1, 0, 0], -.5)
+pCarry = Perceptron([0, 1, 1], -1.5)
+layer3 = Layer([pAnd, pNand, pOr])
+layer4 = Layer([pSum, pCarry])
+network2 = Network([layer3, layer4])
+
+assert network2.predict([1, 1]) == [1, 0]
+assert network2.predict([1, 0]) == [0, 1]
+assert network2.predict([0, 1]) == [0, 1]
+assert network2.predict([0, 0]) == [0, 0]
---
+++ neural.py
@@ -0,0 +1,63 @@
+import math
+
+
+class Perceptron:
+    def __init__(self, weights: list, bias: float):
+        self.weights = weights
+        self.bias = bias
+        self.threshold = -bias
+
+    def sum(self, inputs: list) -> float:
+        activation = self.bias
+        for i in range(0, len(inputs)):
+            activation += inputs[i] * self.weights[i]
+        return activation
+
+
+    def activation(self, inputs: list) -> int:
+        if(len(inputs) != len(self.weights)):
+            raise Exception("Inputs and weights do not have the same shape.")
+        if(self.sum(inputs) >= .5):
+            return 1
+        return 0
+    
+    def __str__(self) -> str:
+        return "Perceptron weight: " + str(self.weights) + "\n" + "Perceptron bias: " + str(self.bias) + "\n"
+
+class Layer: 
+    def __init__(self, perceptrons: list[Perceptron]):
+        self.perceptrons = perceptrons
+
+    def activation(self, inputs: list):
+        return [x.activation(inputs) for x in self.perceptrons]
+
+    def __str__(self):
+        layerOutput = "This layer has the following perceptrons: \n"
+        for x in self.perceptrons:
+            layerOutput += str(x)
+        return layerOutput
+
+class Network:
+    def __init__(self, layers: list[Layer]):
+        self.layers = layers
+
+    def add_layer(self, layer: Layer):
+        self.layers.append(layer)
+
+    def feed_forward(self, inputs: list, prevLayer: Layer, index: int = 0):
+        activation = prevLayer.activation(inputs)
+        index += 1
+        if index < len(self.layers):
+            return self.feed_forward(activation, self.layers[index], index)
+            
+        return activation
+
+    def predict(self, inputs):
+        return self.feed_forward(inputs, self.layers[0])
+
+    def __str__(self):
+        networkOutput = "Below you will find detailed information about this networks layers: \n\n"
+        for layer in self.layers:
+            networkOutput += str(layer)
+        return networkOutput
+
