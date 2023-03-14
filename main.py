468487ee65a36e0facf2dbba0c467f4f4c16447a
Joey1999 <joeywelvaadt@gmail.com>
2023-03-14 02:44:23 +0100

    P2 and P3, updated tests

--- main.py
+++ main.py
@@ -1,62 +1,268 @@
-from neural import Layer, Network, Perceptron
+from neural import Perceptron, Neuron, Layer, Network, Vizualize, NeuronLayer, NeuronNetwork
 
-### Starting Assertion ###
-# Invert
-pInvert = Perceptron([-1], 1)
-assert pInvert.activation([0]) == 1
-assert pInvert.activation([1]) == 0
+#### Perceptron P1 ####
+print('\n\nStarting P1\n\n')
 
-# AND
-pAnd = Perceptron([1, 1], -1)
-assert pAnd.activation([1, 1]) == 1
-assert pAnd.activation([1, 0]) == 0
-assert pAnd.activation([0, 1]) == 0
-assert pAnd.activation([0, 0]) == 0
+## Defining print function for tests ##
+def print_test(object, inputs: list[int], answers: list[int]):
+    for i in range(0, len(inputs)):
+        activation:int = object.activate(inputs[i])
+        if(activation == answers[i]):
+            print("The activation with inputs", inputs[i], "gave the correct answer which was:", activation, '\n')
+        else:
+            print("The activation with inputs", inputs[i], "gave the wrong answer which was:", activation, "it should be:", answers[i], '\n')
+    print(object)
 
-# OR
-pOr = Perceptron([1, 1], -0.5)
-assert pOr.activation([1, 1]) == 1
-assert pOr.activation([1, 0]) == 1
-assert pOr.activation([0, 1]) == 1
-assert pOr.activation([0, 0]) == 0
+def print_test_neuron(neuron:Neuron, inputs: list[int], answers: list[int]):
+    
+    for i in range(0, len(inputs)):
+        activation:float = neuron.activate(inputs[i])
+        print(inputs[i], activation)
 
-# Extended
-pExt = Perceptron([0.6, 0.3, 0.2], -0.4)
-assert pExt.activation([1,1,1]) == 1
-assert pExt.activation([1,1,0]) == 0
-assert pExt.activation([1,0,0]) == 0
-assert pExt.activation([1,0,1]) == 0
-assert pExt.activation([0,1,1]) == 0
-assert pExt.activation([0,1,0]) == 0
-assert pExt.activation([0,0,1]) == 0
-assert pExt.activation([0,0,0]) == 0
+def print_test_neuron_network(network: Network, inputs: list[int], answers: list[int]):
+    for i in range(len(inputs)):
+        activations = network.activate(inputs[i])
+        for j in range(len(activations)):
+            activations[j] = 1 if activations[j] >= 0.5 else 0
+        if(activations == answers[i]):
+            print("The activation with inputs", inputs[i], "gave the correct answer which was:", activations, '\n')
+        else:
+            print("The activation with inputs", inputs[i], "gave the wrong answer which was:", activations, "it should be:", answers[i], '\n')
+    print(network)
 
-# NAND
-pNand = Perceptron([-1,-1], 2)
-assert pNand.activation([1,1]) == 0
-assert pNand.activation([1,0]) == 1
-assert pNand.activation([0,1]) == 1
-assert pNand.activation([0,0]) == 1
 
-### Starting network test ###
-# XOR
-layer1 = Layer([pNand, pOr])
-layer2 = Layer([pAnd])
-network = Network([layer1, layer2])
+# Defining INVERT perceptron #
+invert_perceptron = Perceptron([-1], 0)
+invert_inputs = [
+    [1],
+    [0]
+]
+invert_answers = [
+    0,
+    1
+]
+print("\n\nINVERT Perceptron\n")
+print_test(invert_perceptron, invert_inputs, invert_answers)
 
-assert network.predict([1, 1]) == [0]
-assert network.predict([1, 0]) == [1]
-assert network.predict([0, 1]) == [1]
-assert network.predict([0, 0]) == [0]
+# Defining inputs for all perceptrons with 2 inputs #
+perceptron_inputs = [
+    [1, 1],
+    [1, 0],
+    [0, 1],
+    [0, 0]
+]
 
-#Half adder
-pSum = Perceptron([1, 0, 0], -.5)
-pCarry = Perceptron([0, 1, 1], -1.5)
-layer3 = Layer([pAnd, pNand, pOr])
-layer4 = Layer([pSum, pCarry])
-network2 = Network([layer3, layer4])
-print(network2)
-assert network2.predict([1, 1]) == [1, 0]
-assert network2.predict([1, 0]) == [0, 1]
-assert network2.predict([0, 1]) == [0, 1]
-assert network2.predict([0, 0]) == [0, 0]
+# Defining AND perceptron #
+and_perceptron = Perceptron([0.5, 0.5], -1)
+and_answers = [
+    1,
+    0,
+    0,
+    0
+]
+
+print("\n\nAND Perceptron\n")
+print_test(and_perceptron, perceptron_inputs, and_answers)
+
+# Defining OR perceptron #
+or_perceptron = Perceptron([1, 1], -1)
+or_answers = [
+    1,
+    1,
+    1,
+    0
+]
+
+print("\n\nOR Perceptron\n")
+print_test(or_perceptron, perceptron_inputs, or_answers)
+
+
+# Defining NAND perceptron #
+nand_perceptron = Perceptron([-1, -1], 1.5)
+nand_answers = [
+    0,
+    1,
+    1,
+    1
+]
+
+print("\n\nNAND Perceptron\n")
+print_test(nand_perceptron, perceptron_inputs, nand_answers)
+
+# Defining 3 Input perceptron #
+three_perceptron = Perceptron([0.6, 0.3, 0.2], -0.4)
+three_inputs = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 0, 0],
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0]
+]
+three_answers = [
+    1,
+    1,
+    0,
+    0,
+    1,
+    1,
+    1,
+    0
+]
+
+print("\n\n3 Input Perceptron\n")
+print_test(three_perceptron, three_inputs, three_answers)
+
+# Defining NOR perceptron #
+nor_perceptron = Perceptron([-1, -1, -1], 0)
+nor_answers = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1
+]
+
+print("\n\nNOR Perceptron\n")
+print_test(nor_perceptron, three_inputs, nor_answers)
+
+
+
+# Defining XOR network #
+layer_xor_in: Layer = Layer([or_perceptron, nand_perceptron], 2, 2)
+layer_xor_out: Layer = Layer([and_perceptron], 2, 1)
+
+network_xor = Network(2)
+network_xor.add_layer(layer_xor_in)
+network_xor.add_layer(layer_xor_out)
+
+network_xor_inputs = [
+    [1, 1],
+    [1, 0],
+    [0, 1],
+    [0, 0]
+]
+
+network_xor_answers = [
+    [0],
+    [1],
+    [1],
+    [0]
+]
+
+print("\n\nXOR Network\n")
+print_test(network_xor, network_xor_inputs, network_xor_answers)
+
+# Defining Half Adder network #
+sum_perceptron = Perceptron([1, 0, 0], -.5)
+carry_perceptron = Perceptron([-3, 1, 1], -.5)
+layer_ha_in = Layer([and_perceptron, and_perceptron, or_perceptron], 2, 3)
+layer_ha_out = Layer([sum_perceptron, carry_perceptron], 3, 2)
+
+network_ha = Network(2)
+network_ha.add_layer(layer_ha_in)
+network_ha.add_layer(layer_ha_out)
+
+network_ha_inputs = [
+    [1, 1],
+    [1, 0],
+    [0, 1],
+    [0, 0]
+]
+
+network_ha_answers = [
+    [1, 0],
+    [0, 1],
+    [0, 1],
+    [0, 0]
+]
+print("\n\nHalf Adder Network\n")
+print_test(network_ha, network_ha_inputs, network_ha_answers)
+
+#### Perceptron learning rule P2 ####
+print('\n\nStarting P2\n\n')
+print("View notebook: P2 - Learning rule test")
+
+#### Sigmoid Neuron P3 ####
+print('\n\nStarting P3\n\n')
+
+# Defining INVERT neuron #
+#Invert Perceptron: ([-1], 0.5)
+invert_neuron = Neuron([-1], 0.5)
+
+print("\n\nINVERT Neuron (With Perceptron inputs)\n")
+print_test_neuron(invert_neuron, invert_inputs, invert_answers)
+print("\nINVERT Neuron Anaylze: Using the same inputs as were used in the perceptron. We get the outputs seen above: 0.3775 for input 1 and 0.6224 for input 0. \nWe can conclude that these inputs work for this particular case, because a sigmoid neuron fires when the output is above or equal to 0.5. \nSo to sum up, the neuron fires when the input is 0 and doesn't fire when the input is 1. \n")
+
+
+# Defining AND neuron #
+#And Perceptron: ([0.5, 0.5], -1)
+and_neuron = Neuron([0.5, 0.5], -1)
+
+print("\n\nAND Neuron (With Perceptron inputs)\n")
+print_test_neuron(and_neuron, perceptron_inputs, and_answers)
+print("\nAND Neuron Anaylze: As mentioned int the Analyze above, a sigmoid neuron fires when the output is greater or equal to 0.5. \nIn the outputs above only the input [1, 1] matches this requirement. This means the neuron also works with the AND percoptron weights and bias. \n")
+
+
+#Defining OR neuron #
+#Or Perceptron: ([1, 1], -1)
+or_neuron = Neuron([1, 1], -1)
+
+print("\n\nOR Neuron (With Perceptron inputs)\n")
+print_test_neuron(or_neuron, perceptron_inputs, or_answers)
+print("\nOR Neuron Anaylze: The OR neuron also seems to work as it should, it fires with the inputs: [1,1], [1,0] and [0,1]. This is exactly what should happen with an OR gate. \n")
+
+#Defining NOR neuron #
+nor_neuron = Neuron([-1, -1, -1], 0)
+
+print("\n\nNOR Neuron (With Perceptron inputs)\n")
+print_test_neuron(nor_neuron, three_inputs, nor_answers)
+
+# Defining Half Adder neuron network #
+sum_neuron = Neuron([1, 0, 0], -.5)
+carry_neuron =  Neuron([-3, 1, 1], -.5)
+layer_ha_neuron_in = NeuronLayer([and_neuron, and_neuron, or_neuron], 2, 3)
+layer_ha_neuron_out = NeuronLayer([sum_neuron, carry_neuron], 3, 2)
+
+network_ha_neuron = NeuronNetwork(2)
+network_ha_neuron.add_layer(layer_ha_neuron_in)
+network_ha_neuron.add_layer(layer_ha_neuron_out)
+
+print("\n\nHalf Adder Neuron Network\n")
+print_test_neuron_network(network_ha_neuron, network_ha_inputs, network_ha_answers)
+# Defining inputs for all perceptrons with 2 inputs #
+# perceptron_inputs = [
+#     [1, 1],
+#     [1, 0],
+#     [0, 1],
+#     [0, 0]
+# ]
+
+# # Defining AND perceptron #
+# and_perceptron = Perceptron([0.5, 0.5], -0.5)
+# and_answers = [
+#     1,
+#     0,
+#     0,
+#     0
+# ]
+
+# print("\n\nAND Perceptron\n")
+# print_test(and_perceptron, perceptron_inputs, and_answers)
+
+# # Defining OR perceptron #
+# or_perceptron = Perceptron([1, 1], -0.5)
+# or_answers = [
+#     1,
+#     1,
+#     1,
+#     0
+# ]
+
+# print("\n\nOR Perceptron\n")
+# print_test(or_perceptron, perceptron_inputs, or_answers)
--- neural.py
+++ neural.py
@@ -1,64 +1,197 @@
 import math
+from tkinter import *
+from tkinter import ttk
+
+
+#Class containing all different possible activation methods
+class Activation:
+    #Step function assuming we are using a bias so the threshold is 0.5
+    def step(x) -> int:
+        if(x >= 0):
+            return 1
+        return 0
+
+    #Sigmoid function
+    def sigmoid(x: float) -> float:
+        #source: https://en.wikipedia.org/wiki/Sigmoid_function
+        return 1/(1 + (math.e**-x))
+
+#Class containing the neuron information
+class Neuron:
+    def __init__(self, weights: list[float], bias: float):
+        self.weights = weights
+        self.bias = bias
+
+    def activate(self, inputs: list[float]):
+        #Map through the inputs and multiply them by the weights. Add bias to the total sum of this.
+        activation_sum: float = sum([self.weights[i] * inputs[i] for i in range(0, len(inputs))]) + self.bias
+        #Pass the activation_sum to the activation function.
+        return Activation.sigmoid(activation_sum)
+    
+    #Output the neuron in a nice way in the console.
+    def __str__(self) -> str:
+        return f"Neuron(weights={self.weights}, bias={self.bias})"
+    
+#Class containing all neurons in a layer.
+class NeuronLayer():
+    def __init__(self, neurons: list[Neuron], inputs: int, outputs: int):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.neurons = neurons
+
+    def activate(self, inputs: list[float]) -> list[float]:
+        #Activate all the perceptrons with the inputs given.
+        return [p.activate(inputs) for p in self.neurons]
+    
+    #Output the layer in a nice way in the console.
+    def __str__(self) -> str:
+        return f'Layer with {self.inputs} inputs and {self.outputs} outputs.'
+
+#Class containing all layers of a network.
+class NeuronNetwork():
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.layers = []
+
+    def add_layer(self, layer: NeuronLayer) -> None:
+        self.layers.append(layer)
+
+    
+    def activate(self, inputs: list[float]) -> list[float]:
+        #Loop through the layers
+        for layer in self.layers:
+            #Set inputs to the layer response so we can feed forward
+            inputs = layer.activate(inputs)
+        return inputs
+    
+    def __str__(self) -> str:
+        return f'Network with {self.inputs} inputs and {len(self.layers)} layers'
 
 
 class Perceptron:
-    def __init__(self, weights: list, bias: float):
+    def __init__(self, weights: list[float], bias: float):
         self.weights = weights
         self.bias = bias
-        self.threshold = -bias
 
-    def sum(self, inputs: list) -> float:
-        activation = self.bias
-        for i in range(0, len(inputs)):
-            activation += inputs[i] * self.weights[i]
-        return activation
+    def activate(self, inputs: list[float]) -> float:
+        #Map through the inputs and multiply them by the weights. Add bias to the total sum of this.
+        activation_sum: float = sum([self.weights[i] * inputs[i] for i in range(0, len(inputs))]) + self.bias
+        #Pass the activation_sum to the activation function.
+        return Activation.step(activation_sum)
+
+    #Defining update function that implements perceptron learning rule
+    def update(self, inputs: list[float], target: float):
+        #Learning rate
+        eta = 0.1
+
+        #Output
+        y = self.activate(inputs)
+
+        #Error
+        e = target - y
+
+        #Delta weigths
+        dw = [eta * e * x for x in inputs]
+        self.weights = [w + dw for w,dw in zip(self.weights,dw)]
+
+        #Delta bias
+        db = eta * e
+        self.bias += db 
 
 
-    def activation(self, inputs: list) -> int:
-        if(len(inputs) != len(self.weights)):
-            raise Exception("Inputs and weights do not have the same shape.")
-        if(self.sum(inputs) >= .5):
-            return 1
-        return 0
-    
+    def loss(self, examples):
+        n = len(examples)
+        total_loss = 0.0
+        for example in examples:
+            inputs, target = example
+            output = self.activate(inputs)
+            loss = (target - output) ** 2
+            total_loss += loss
+        mse = total_loss / n
+        return mse
+
+    #Output the perceptron in a nice way in the console.
     def __str__(self) -> str:
-        return "Perceptron weight: " + str(self.weights) + "\n" + "Perceptron bias: " + str(self.bias) + "\n"
-
-class Layer: 
-    def __init__(self, perceptrons: list[Perceptron], name: str):
+        return f"Perceptron(weights={self.weights}, bias={self.bias})"
+    
+class Layer:
+    def __init__(self, perceptrons: list[Perceptron], inputs: int, outputs: int):
+        self.inputs = inputs
+        self.outputs = outputs
         self.perceptrons = perceptrons
-        self.name = name
 
-    def activation(self, inputs: list):
-        return [x.activation(inputs) for x in self.perceptrons]
-
-    def __str__(self):
-        layerOutput = "The layer "+ self.name +" has the following perceptrons: \n"
-        for x in self.perceptrons:
-            layerOutput += str(x)
-        return layerOutput
+    def activate(self, inputs: list[float]) -> list[float]:
+        #Activate all the perceptrons with the inputs given.
+        return [p.activate(inputs) for p in self.perceptrons]
+    
+    #Output the layer in a nice way in the console.
+    def __str__(self) -> str:
+        return f'Layer with {self.inputs} inputs and {self.outputs} outputs.'
 
 class Network:
-    def __init__(self, layers: list[Layer]):
-        self.layers = layers
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.layers = []
 
-    def add_layer(self, layer: Layer):
+    def add_layer(self, layer: Layer) -> None:
         self.layers.append(layer)
 
-    def feed_forward(self, inputs: list, prevLayer: Layer, index: int = 0):
-        activation = prevLayer.activation(inputs)
-        index += 1
-        if index < len(self.layers):
-            return self.feed_forward(activation, self.layers[index], index)
-            
-        return activation
-
-    def predict(self, inputs):
-        return self.feed_forward(inputs, self.layers[0])
-
-    def __str__(self):
-        networkOutput = "Below you will find detailed information about this networks layers: \n\n"
+    
+    def activate(self, inputs: list[float]) -> list[float]:
+        #Loop through the layers
         for layer in self.layers:
-            networkOutput += str(layer)
-        return networkOutput
+            #Set inputs to the layer response so we can feed forward
+            inputs = layer.activate(inputs)
+        return inputs
+    
+    def __str__(self) -> str:
+        return f'Network with {self.inputs} inputs and {len(self.layers)} layers'
+    
+
+class Vizualize: 
+    def __init__(self, network: Network, inputs: list):
+        self.root = Tk()
+        self.network = network
+        self.frame = ttk.Frame(self.root)
+        self.width, self.height = (self.root.winfo_screenwidth(), self.root.winfo_screenheight())
+        self.canvas = Canvas(self.root, width=self.width, height=self.height, borderwidth=0, highlightthickness=0,
+                   bg="black")
+        
+        self.canvas.grid()
+        self.draw_layers()
+        self.draw_connections()
+        self.root.geometry(f"{self.width}x{self.height}")
+        self.root.mainloop()
+
+    def create_circle(self, x, y, r, **kwargs):
+            return self.canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+
+    def draw_connections(self):
+        for row in range(len(self.points)):
+            for col in range(len(self.points[row])):
+                if(row + 1 < len(self.points)):
+                    for nextCol in self.points[row+1]:
+                        x1, y1 = self.points[row][col]
+                        x2, y2 = nextCol 
+                        self.canvas.create_line(x1, y1, x2, y2, fill="#fff")
+
+    def draw_layers(self):
+        layers: list[Layer] = self.network.layers
+        inputLayerPerceptrons: int = layers[0].inputs
+        self.points = [[] for x in [*layers, Layer([], 0, 0)]]
+        print (self.points)
+        for i in range(inputLayerPerceptrons):
+            x: int = (self.width / 2) - (((len(layers) + 1) / 2) * 100)
+            y: int = (self.height / 2 - ((inputLayerPerceptrons / 2) * 100)) + (100 * i)
+            self.create_circle(x, y, 20, fill="blue", outline="#DDD", width=4)
+            self.points[0].append((x,y))
+
+        for i in range(len(layers)):
+            for j in range(len(layers[i].perceptrons)):
+                x: int = (self.width / 2) - (((len(layers) + 1) / 2) * 100) + (100 * (i + 1))
+                y: int = (self.height / 2 - ((len(layers[i].perceptrons) / 2) * 100)) + (100 * j)
+                self.create_circle(x, y, 20, fill="blue", outline="#DDD", width=4)
+                
+                self.points[i+1].append((x,y))
+        pass
 
