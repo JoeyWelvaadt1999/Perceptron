f9c9df85ecc084ff5cdd538808cb4c6ef9403033
Joey1999 <joeywelvaadt@gmail.com>
2023-03-19 00:32:44 +0100

    started backpropagation

--- neural.py
+++ neural.py
@@ -1,4 +1,5 @@
 import math
+from typing import Tuple
 from tkinter import *
 from tkinter import ttk
 
@@ -18,16 +19,73 @@ class Activation:
 
 #Class containing the neuron information
 class Neuron:
-    def __init__(self, weights: list[float], bias: float):
+    def __init__(self, weights: list[float], bias: float, index: int = 0):
         self.weights = weights
         self.bias = bias
+        self.index = index
 
     def activate(self, inputs: list[float]):
+        #Saving inputs for weight delta
+        self.inputs = inputs
         #Map through the inputs and multiply them by the weights. Add bias to the total sum of this.
         activation_sum: float = sum([self.weights[i] * inputs[i] for i in range(0, len(inputs))]) + self.bias
+
+        #Save the activation to be used later in backpropagation
         #Pass the activation_sum to the activation function.
-        return Activation.sigmoid(activation_sum)
+        self.output = Activation.sigmoid(activation_sum)
+        return self.output
     
+    #Calculating neuron output error
+    def output_error(self, target: float) -> float:
+        #σ'(inputj) = outputj ∙ (1 – outputj)
+        sigmoid_deriv = self.output * (1 - self.output)
+        
+        #σ'(inputj) ∙ –(targetj – outputj)
+        self.o_error = sigmoid_deriv * -(target - self.output)
+        return self.o_error
+    
+    #Calculating hidden neuron error
+    def hidden_error(self, next_layer_neurons: list) -> float:
+        next_layer_neurons: list[Neuron] = next_layer_neurons
+        #σ'(inputi)
+        sigmoid_deriv = self.output * (1 - self.output)
+        #Δi = σ'(inputi) ∙ Σj wi,j ∙ Δj
+        error_sum: float = sum([neuron.weights[self.index] * neuron.o_error for neuron in next_layer_neurons])
+        self.h_error = sigmoid_deriv * error_sum
+        return self.h_error
+    
+    #Calculating the gradient of a single weight
+    def weight_gradient(self, input: float):
+        #∂C/∂wi,j = outputi ∙ Δj
+        return input * self.o_error
+    
+    #Calculating the delta of all weights
+    def weight_deltas(self, eta: float) -> list[float]:
+        #Δwi,j = η ∙ ∂C/∂wi,j = η ∙ outputi ∙ Δj
+        d_weights = [eta * self.weight_gradient(self.inputs[i]) for i in range(len(self.weights))]
+        self.d_weights = d_weights
+        return self.d_weights
+
+    #Calculating the delta of the bias
+    def bias_delta(self, eta: float) -> float:
+        #Δbj = η ∙ Δj
+        self.d_bias = eta * self.o_error
+        return self.d_bias
+
+    #Updating the neurons by applying the weights and bias deltas
+    def update(self) -> Tuple[list[float], float]:
+        #w'i,j = wi,j – Δwi,j
+        self.weights = [self.weights[i] - self.d_weights[i] for i in range(len(self.weights))]
+
+        #Δbj = η ∙ Δj
+        self.bias -= self.d_bias
+
+        #Resetting delta variables
+        self.d_weights = []
+        self.d_bias = 0
+
+        return (self.weights, self.bias)
+
     #Output the neuron in a nice way in the console.
     def __str__(self) -> str:
         return f"Neuron(weights={self.weights}, bias={self.bias})"
@@ -41,7 +99,16 @@ class NeuronLayer():
 
     def activate(self, inputs: list[float]) -> list[float]:
         #Activate all the perceptrons with the inputs given.
-        return [p.activate(inputs) for p in self.neurons]
+        return [n.activate(inputs) for n in self.neurons]
+    
+    #Calculating all errors of neurons in layer
+    def neuron_errors(self, layer) -> None:
+        [n.hidden_error(layer.neurons) for n in self.neurons]
+
+    def update(self, eta: float) -> None:
+        [n.weight_deltas(eta) for n in self.neurons]
+        [n.bias_delta(eta) for n in self.neurons]
+        [n.update() for n in self.neurons]
     
     #Output the layer in a nice way in the console.
     def __str__(self) -> str:
@@ -64,6 +131,28 @@ class NeuronNetwork():
             inputs = layer.activate(inputs)
         return inputs
     
+    #Running backpropagation through the neural network
+    def backpropagate(self, data: Tuple[list[float], float], eta: float) -> None:
+        #Get inputs and targets from data
+        inputs, target = data
+
+        output_layer: NeuronLayer = self.layers[-1]
+
+        #Activate the network
+        self.activate(inputs)
+        [n.output_error(target) for n in output_layer.neurons]
+
+        #Determining weight deltas
+        for i in range(len(self.layers) - 2, 0, -1):
+            current_layer: NeuronLayer = self.layers[i]
+            current_layer.neuron_errors(self.layers[i+1])
+
+        for i in range(len(self.layers)):
+            current_layer: NeuronLayer = self.layers[i]
+            current_layer.update(eta)
+
+
+
     def __str__(self) -> str:
         return f'Network with {self.inputs} inputs and {len(self.layers)} layers'
 
